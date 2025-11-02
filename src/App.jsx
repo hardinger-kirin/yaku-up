ca8ff97201a100988a2ffd477cc9183a93f15a5e
Kirin Hardinger <89322661+hardinger-kirin@users.noreply.github.com>
2025-11-02 15:17:06 -0800

    Init project
    Setting up Vite and Bootstrap. Simple page that says "Yaku Up!" and a button that does nothing

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div className="container text-center mt-5">
+      <h1 className="mb-4">Yaku Up!</h1>
+      <p>Learn Riichi Mahjong tiles and yaku through interactive flashcards.</p>
+      <button className="btn btn-success mt-3">Start Learning</button>
+    </div>
+  );
+}
+
+export default App;
